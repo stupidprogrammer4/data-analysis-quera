93228f97893750115fdcfeb95363b84d3232f70d
stupidprogrammer4 <captainjoker757@gmail.com>
2025-05-20 15:05:36 +0330

    clean np 1

--- numpy/numpy_iris_flower/calc_no_loop.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-import numpy as np
-def calc_no_loop(new_points, points):
-    m = len(new_points)
-    n = len(points)
-    return np.sum(np.square(points.reshape(1, n, 4) - new_points.reshape(m, 1, 4)), axis=2)
